05bc75d357970572d1c01510bbfad2995c8ba1a0
Jhoana TEDECO <tedecolegal@gmail.com>
2025-04-13 11:37:48 -0500

    Corrijo host y puerto para despliegue en Render

--- app.py
+++ app.py
@@ -42,5 +42,9 @@ def preguntar():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
